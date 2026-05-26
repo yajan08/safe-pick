d0dbbb7915f6dd5cca3e363540a48e70b0af3ba3
yajan08 <yajanmehta@gmail.com>
2026-05-26 17:12:44 +0530

    image logo

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.safepick.safe_pick"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
